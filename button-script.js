c46d8779e5cf26269b6ac0961c386651169b4483
nhzlei <nhi.le@ada.ac.uk>
2025-09-14 22:42:44 +0100

    change file struture plus audio plus responsive btn

--- button-script.js
+++
@@ -1,9 +0,0 @@
-let point = 0;
-const audio = document.getElementById("meowcat1")
-
-function cat_clicked() {
-	point += 1;
-	document.querySelector(".point").textContent = point;
-	audio.currentTime =  0;
-	audio.play();
-}
