08931457a3bfadd16428028e8ace902f75af5428
nhzlei <nhi.le@ada.ac.uk>
2025-09-17 11:37:43 +0100

    updated upgrade

--- main/html/script.js
+++ main/html/script.js
@@ -1,16 +1,16 @@
 let point = 0;
-const audio = document.getElementById("meowcat1")
+const audio1 = document.getElementById("meowcat1")
 
 function cat_clicked() {
 	point += 1;
 	document.querySelector(".point").textContent = point;
-	audio.currentTime =  0;
-	audio.play();
+	audio1.currentTime =  0;
+	audio1.play();
 }
 
 function upgrade1() {
 	point += 2;
 	document.querySelector(".point").textContent = point;
-	audio.currentTime =  0;
-	audio.play();
+	audio1.currentTime =  0;
+	audio1.play();
 }
